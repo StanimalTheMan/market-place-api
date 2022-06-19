3e61ed3e51069f1cdcb2f71c471fa7ef0e200e6e
StanimalTheMan <sjc591@nyu.edu>
2022-06-18 20:04:31 -0400

    Factorize pagination

--- app/controllers/concerns/paginable.rb
+++ app/controllers/concerns/paginable.rb
@@ -1,6 +1,17 @@
 module Paginable
     protected
 
+    def get_links_serializer_options links_path, collection
+        {
+            links: {
+                first: send(links_path, page: 1),
+                last: send(links_path, page: collection.total_pages),
+                prev: send(links_path, page: collection.prev_page),
+                next: send(links_path, page: collection.next_page),
+            }
+        }
+    end
+
     def current_page
         (params[:page] || 1).to_i
     end
